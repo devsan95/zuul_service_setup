9ae30f6230a657261528ce1c30d824b57b183660
devsan95 <vaggasantoshkumar@gmail.com>
2023-01-12 15:00:52 +0530

    gg

--- script.py
+++ script.py
@@ -148,10 +148,9 @@ def make_workspace():
 
 
 def install_java():
-    (stdin, stdout, stderr) = ssh.exec_command('sudo yum -y install wget curl;wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz;tar xvf openjdk-17.0.2_linux-x64_bin.tar.gz;sudo mv jdk-17.0.2/ /opt/jdk-17/')
+    (stdin, stdout, stderr) = ssh.exec_command('sudo yum -y install wget curl; wget https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz;tar xvf openjdk-17.0.2_linux-x64_bin.tar.gz;sudo mv jdk-17.0.2/ /opt/jdk-17/')
     (stdin, stdout, stderr) = ssh.exec_command('java --version')
-    java_ver = stdout.read()
-    if 'openjdk 17.' in str(java_ver):
+    if 'openjdk 17.' in str(stdout.read()):
         logger.info("Java 17 Installed successully.")
     else:
         logger.error(f"Error in Java 17 Installation.{stderr.read()}")
